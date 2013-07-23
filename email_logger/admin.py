f091ed7b61b33183443f28c973b0ea4f439fcbeb
fruitschen <fruitschen@gmail.com>
2013-07-23 15:12:38 +0800

    change admin name

--- email_logger/admin.py
+++ email_logger/admin.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from email_logger.models import EmailLog
 
 class EmailLogAdmin(admin.ModelAdmin):
-    name = 'Website Content'
+    name = 'Tools and Settings'
     list_display = ('label', 'subject', 'sender', 'recipients', 'created')
     search_fields = ('label', 'subject')
 
