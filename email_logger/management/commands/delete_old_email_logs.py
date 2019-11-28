652da755d8471d3e8b7d0c009815bc70979272f5
Chen Zhe <fruitschen@gmail.com>
2019-11-28 15:21:32 +0800

    make RENTATION configurable

--- email_logger/management/commands/delete_old_email_logs.py
+++ email_logger/management/commands/delete_old_email_logs.py
@@ -1,5 +1,6 @@
 from datetime import timedelta
 
+from django.conf import settings
 from django.core.management.base import BaseCommand
 from django.utils import timezone
 from email_logger.models import EmailLog
@@ -9,8 +10,11 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         now = timezone.now()
-        two_years_ago = now - timedelta(days=365*2)
-        retention_time = two_years_ago
+        if getattr(settings, 'AUTO_DELETE_OLD_EMAILLOG_RENTATION_DAYS', None):
+            retention_time = settings.AUTO_DELETE_OLD_EMAILLOG_RENTATION_DAYS
+        else:
+            two_years_ago = now - timedelta(days=365*2)
+            retention_time = two_years_ago
         logs_to_delete = EmailLog.objects.filter(created__lte=retention_time)
         print(
             '{} Email Logs to delete. We have {} email logs in total. '.format(
